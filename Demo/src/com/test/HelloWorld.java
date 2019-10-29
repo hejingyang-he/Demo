e0686ece973193547f1104b339f31c616b780fc8
dabaojian <576579405@qq.com>
2019-10-29 16:50:38 +0800

    jay提交了一次

--- Demo/src/com/test/HelloWorld.java
+++ Demo/src/com/test/HelloWorld.java
@@ -4,7 +4,6 @@ public class HelloWorld {
 public static void main(String[] args) {
 
 	System.out.println("one commit");
-	System.out.println("write");
-	System.out.println("zdx");
+	System.out.println("dabaojian");
 }
 }
