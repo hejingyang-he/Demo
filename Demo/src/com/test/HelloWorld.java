06ee9b625034f2e8303b8d4727234af8c031f3b4
wzf575843 <575843404@qq.com>
2019-10-29 15:52:56 +0800

    小方

--- Demo/src/com/test/HelloWorld.java
+++ Demo/src/com/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.test;
 public class HelloWorld {
 public static void main(String[] args) {
 
-	System.out.println("one commit");
-	System.out.println("write");
+	System.out.println("wuzhenfang");
+	
 }
 }
