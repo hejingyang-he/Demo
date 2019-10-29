7389b7bcbfbcca1f01f689bc7d6f016432ba85fd
luck-Strike <1832380924@qq.com>
2019-10-29 16:54:36 +0800

    788

--- Demo/src/com/test/HelloWorld.java
+++ Demo/src/com/test/HelloWorld.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
 	System.out.println("one commit");
 	System.out.println("write");
 	System.out.println("FanShiWen");
-	System.out.println("789456412");
+	
 }
 }
