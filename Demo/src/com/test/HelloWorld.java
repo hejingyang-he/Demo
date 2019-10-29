95ffe68589c2df9fdac02e88cb23effa6f5b09bc
cai-chen-chen <158726233@qq.com>
2019-10-29 16:12:55 +0800

    123

--- Demo/src/com/test/HelloWorld.java
+++ Demo/src/com/test/HelloWorld.java
@@ -3,11 +3,6 @@ package com.test;
 public class HelloWorld {
 public static void main(String[] args) {
 
-	System.out.println("one commit");
-	System.out.println("gancongpu");
-	System.out.println("writre");
-	System.out.println("one commit ");
-	System.out.println("write");
-	System.out.println("liangzhiqi");
+	System.out.println("ccc");
 }
 }
